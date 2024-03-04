17c81fd185a253161c52c891d412fba72f942586
Yannick Yanga <cleps2012@gmail.com>
2024-03-04 10:12:22 +0100

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={38}
+            height={38}
+          />
+        </Link>
+        <p>&copy;2024 Koniya Event. All right reserved.</p>
+      </div>
+    </footer>
+  );
 };
